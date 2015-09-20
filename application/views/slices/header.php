1eeac3cccbf0971c5d7946a179e2c7847fc3392f
Erikson Reppetto <erikson.souza@gmail.com>
2015-09-20 19:03:29 -0300

    Atualizada a versão do Bootstrap e do jQuery

--- application/views/slices/header.php
+++ application/views/slices/header.php
@@ -9,8 +9,8 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Receitas</title>
         <link rel="stylesheet" href="<?= base_url('front_end/css/geral.css') ?>">
-        <link rel="stylesheet" href="<?= base_url('front_end/css/bootstrap-responsive.min.css') ?>">
-        <script type="text/javascript" src="<?= base_url('front_end/js/jquery-1.10.2.js') ?>"></script>
+        <link rel="stylesheet" href="<?= base_url('front_end/css/bootstrap.min.css') ?>">
+        <script type="text/javascript" src="<?= base_url('front_end/js/jquery-2.1.4.min.js') ?>"></script>
         <script type="text/javascript" src="<?= base_url('front_end/js/bootstrap.min.js') ?>"></script>
         <script type="text/javascript" src="<?= base_url('front_end/js/geral.js') ?>"></script>
     </head>

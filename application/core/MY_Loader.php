938583e0a44149dd2e8bc1d0d1f566bf57918ebe
Erikson Reppetto <erikson.souza@gmail.com>
2015-09-20 18:09:24 -0300

    Criado o loader customizado de views

---
+++ application/core/MY_Loader.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class MY_Loader extends CI_Loader {
+    
+    public function template($view, $menu=null, $dados=null) {
+        //$this->view("slices/header");
+        if($menu){
+            if(isset($dados['grupos'])){
+                $this->view("slices/menus/menu_{$menu}", $dados);
+            }else{
+                $this->view("slices/menus/menu_{$menu}");
+            }
+	        
+	    }
+	    if(is_array($view)){
+	    	foreach ($view as $v) {
+	    		$this->view("$v", $dados);
+	    	}
+	    }else{
+        	$this->view("$view", $dados);
+    	}
+        //$this->view("slices/footer");
+    }
+}

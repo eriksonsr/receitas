71d9be829d6f65ffe212a6f894e89b02bb0e8b91
Erikson Reppetto <erikson.souza@gmail.com>
2015-10-04 21:29:23 -0300

    Busca de receita funcionando

--- application/controllers/Receitas.php
+++ application/controllers/Receitas.php
@@ -3,11 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Receitas extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+
+        $this->load->model('Receitas_model','receitas');
+        
+    }
+
 	public function buscaReceita(){
 		$this->load->template('receitas/busca_receita.php');
 	}
 
 	public function resultadoBuascaReceita(){
+		$ingredientes = $this->input->post();
+		
+		$result = $this->receitas->buscaReceita($ingredientes);
+		echo "<pre>";
+		print_r($result);
 		
 	}
 }
